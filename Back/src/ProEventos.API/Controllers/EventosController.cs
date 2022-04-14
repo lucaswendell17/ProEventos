59b74db92781b655e0b9ac87066cc81d2218c675
Lucas Wendel <lucas.wmartins@hotmail.com>
2022-04-13 23:40:59 -0300

    Seção 5: .Net 5 - Múltiplas Camadas

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using ProEventos.API.Data;
-using ProEventos.API.Models;
+using ProEventos.Domain;
+using ProEventos.Persistence.Contextos;
 
 namespace ProEventos.API.Controllers
 {
@@ -14,8 +13,8 @@ namespace ProEventos.API.Controllers
     public class EventosController : ControllerBase
     {
 
-        private readonly DataContext _context;
-        public EventosController(DataContext context)
+        private readonly ProEventosContext _context;
+        public EventosController(ProEventosContext context)
         {
             _context = context;
         }
@@ -29,7 +28,7 @@ namespace ProEventos.API.Controllers
         [HttpGet("{id}")]
         public Evento GetById(int id)
         {
-            return _context.Eventos.FirstOrDefault(e => e.EventoId == id );
+            return _context.Eventos.FirstOrDefault(e => e.Id == id );
         }
     }
 }
