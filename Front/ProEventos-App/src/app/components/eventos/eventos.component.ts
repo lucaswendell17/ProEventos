f50748363ab57995cf25c339ebb8ae6dbb5d1179
Lucas Wendel <lucas.wmartins@hotmail.com>
2022-04-21 22:41:19 -0300

    Seção 9: continuando

--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Evento } from '../models/Evento';
-import { EventoService } from '../services/evento.service';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { Evento } from '../../models/Evento';
+import { EventoService } from '../../services/evento.service';
 
 @Component({
   selector: 'app-eventos',
@@ -9,7 +12,7 @@ import { EventoService } from '../services/evento.service';
   //providers: [EventoService]
 })
 export class EventosComponent implements OnInit {
-
+  modalRef?: BsModalRef;
   public eventos : Evento[] = [];
   public eventosFiltrados : Evento[] = [];
 
@@ -27,10 +30,17 @@ export class EventosComponent implements OnInit {
     this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos
   }
 
-  constructor(private eventoService: EventoService) { }
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+    ) { }
 
   public ngOnInit() : void {
+    this.spinner.show();
     this.getEventos();
+
   }
 
   public filtrarEventos(filtrarPor: string) : Evento[] {
@@ -47,13 +57,30 @@ export class EventosComponent implements OnInit {
 
   public getEventos() : void {
 
-    this.eventoService.getEventos().subscribe(
-      (eventosResp: Evento[]) => {
+    this.eventoService.getEventos().subscribe({
+      next: (eventosResp: Evento[]) => {
         this.eventos = eventosResp;
         this.eventosFiltrados = this.eventos;
       },
-      error => console.log(error)
-    );
+      error: (error: any) => {
+        this.spinner.hide()
+        this.toastr.error('Erro ao carregar os Eventos.', 'Erro!')
+      },
+      complete: () => this.spinner.hide()
+    });
+  }
+
+  openModal(template: TemplateRef<any>) {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+    this.toastr.success('O Evento foi deletado com Sucesso.', 'Deletado');
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
   }
 
 }
